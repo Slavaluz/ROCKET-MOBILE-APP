30f215d96936f7ed65e65bf439921ab20d517b86
Admin <s-luzgin@mail.ru>
2020-03-26 00:38:43 +0800

    Недоделанная первая форма

---
+++ app/src/main/java/com/example/rocket_mobile_app/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.rocket_mobile_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
